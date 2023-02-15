e00ce88804340e2c498120e43dc7d0688d25fa90
Anupam <65004586+Darkzx07@users.noreply.github.com>
2023-02-15 21:38:41 +0530

    Add files via upload

---
+++ Tribute Page/script.js
@@ -0,0 +1,7 @@
+var typed=new Typed(".typing",{
+    strings:["Missile Man of India","An Inspiration","President of India",
+    "Aerospace Scientist"],
+    typeSpeed:100,
+    backSpeed: 80,
+    loop:true,
+});
